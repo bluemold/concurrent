166ab359a1f6d74010c1ce8077bd004d54086649
unknown <Neil@.(none)>
2011-06-05 11:27:41 -0700

    Updated test and fixed some pom configs

---
+++ src/test/scala/bluemold.concurrent/AtomicBooleanTest.scala
@@ -0,0 +1,33 @@
+package bluemold.concurrent;
+
+import junit.framework._;
+import Assert._;
+
+object AtomicBooleanTest {
+    def suite: Test = {
+        val suite = new TestSuite(classOf[AtomicBooleanTest]);
+        suite
+    }
+
+    def main(args : Array[String]) {
+        junit.textui.TestRunner.run(suite);
+    }
+}
+
+/**
+ * Unit test for simple App.
+ */
+class AtomicBooleanTest extends TestCase("app") {
+
+    /**
+     * Rigourous Tests :-)
+     */
+    def testBasics() {
+      val a1 = AtomicBoolean.create()
+      assertFalse(a1.get());
+      val a2 = AtomicBoolean.create( false )
+      assertFalse(a2.get());
+      val a3 = AtomicBoolean.create( true )
+      assertTrue(a3.get());
+    }
+}
